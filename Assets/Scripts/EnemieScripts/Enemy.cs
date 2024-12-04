78f3bbc7c7c39534580ce130514ec580e73e8bd4
Kirill <kmonyov@mail.ru>
2024-12-04 20:26:55 +0300

    Start scripts for enemy object were created. EnemyScene was modified for walking

---
+++ Assets/Scripts/EnemieScripts/Enemy.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int HP = 100;
+    [SerializeField]
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        HP -= damageAmount;
+
+        if (HP <= 0)
+            animator.SetTrigger("Die");
+        else
+            animator.SetTrigger("TakeDamage");
+    }
+}
