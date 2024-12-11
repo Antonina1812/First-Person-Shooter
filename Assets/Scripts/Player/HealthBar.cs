1dc7414fa4d61661a07920526ed64a8d81186d9c
Masterfar7 <145840792+Masterfar7@users.noreply.github.com>
2024-12-11 22:46:12 +0300

    keep working on menu now have some bugs

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -8,15 +8,28 @@ public class HealthBar : MonoBehaviour
     private float health = 100f;
     public Image healthBar;
     public Text healthText;
+    private bool isMenuActive = false;
+    public GameObject pause;
     void Start()
     {
-
+        pause.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         healthText.text=health.ToString();
+        if(Input.GetKeyDown(KeyCode.T) && !isMenuActive)
+        {
+            Debug.Log("Yes");
+            pause2.SetActive(true);
+            isMenuActive = true;
+        }
+        if (Input.GetKeyDown(KeyCode.T) && isMenuActive)
+        {
+            pause.SetActive(false);
+            isMenuActive = false;
+        }
     }
     public void TakeDamage(float damage)
     {
